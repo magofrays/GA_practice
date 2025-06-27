66e229cf074356f6fe69809c18d0552d3a26be75
magofrays <vadimpozdeev2005@gmail.com>
2025-06-27 12:13:22 +0300

    work pls

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
-from include.geneticAlgorithm import geneticAlgorithm
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent / "include"))
+
+from geneticAlgorithm import geneticAlgorithm
 
 if __name__ == '__main__':
     a = geneticAlgorithm()
     a.read_tasks()
     a.create_individuals()
+    a.do_selection()
+    
 
