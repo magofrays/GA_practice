9502d0d2517b288e270d4e1b49617acbc01e9541
Mark Markov <144445462+sxeDexs@users.noreply.github.com>
2025-06-30 14:49:52 +0300

    app style

--- src/include/GUI/app.py
+++ src/include/GUI/app.py
@@ -1,15 +1,17 @@
+import ttkbootstrap as ttkb
 from geneticAlgorithm import geneticAlgorithm
-import tkinter as tk
 from startState import StartState
 from parser import Parser
 from selection import *
 from crossbreeding import *
 from mutation import *
-
 import random
+
+
 class App:
     def __init__(self):
-        self.root = tk.Tk()
+        self.root = ttkb.Window(themename="morph")
+
         self.parser = Parser()
         self.state = StartState(self.root, self)
         self.genAlgorithm = geneticAlgorithm()
@@ -24,10 +26,10 @@ class App:
     def add_tasks(self, *args):
         tasks = self.parser.get_tasks(*args)
         self.genAlgorithm.set_tasks(tasks)
-    
+
     def change_alg_params(self, *args):
         self.genAlgorithm.change_params(*args)
-    
+
     def change_selection_type(self, type: str):
         if type == "TournamentSelection":
             self.genAlgorithm.selection = TournamentSelection()
@@ -35,11 +37,11 @@ class App:
             self.genAlgorithm.selection = RankSelection()
         elif type == "StochasticUniversalSampling":
             self.genAlgorithm.selection = StochasticUniversalSampling()
-    
+
     def change_crossbreeding_type(self, type: str):
         if type == "OrderCrossbreeding":
             self.genAlgorithm.crossbreeding = OrderCrossbreeding()
-    
+
     def change_mutation_type(self, type: str):
         if type == "NoMutation":
             self.genAlgorithm.mutation = NoMutation()
@@ -47,7 +49,7 @@ class App:
             self.genAlgorithm.mutation = SwapMutation()
         elif type == "InversionMutation":
             self.genAlgorithm.mutation = InversionMutation()
-    
+
     def visual_settings(self):
         self.root.geometry("1200x800")
         self.root.title("Генетический алгоритм")
