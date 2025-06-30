e43137c0ab85e4aa5fc47ae948d8d291208539b4
Mark Markov <144445462+sxeDexs@users.noreply.github.com>
2025-06-30 14:50:16 +0300

    workstate style

--- src/include/GUI/workState/workState.py
+++ src/include/GUI/workState/workState.py
@@ -1,49 +1,35 @@
-import tkinter as tk
-from tkinter import ttk, filedialog, messagebox, scrolledtext
+import ttkbootstrap as ttkb
 from scheduleFrame import ScheduleFrame
 from graphView import GraphView
+
 
 class WorkState:
     def __init__(self, root, app):
         self.app = app
         self.root = root
-        
+
     def run(self):
-        self.main_window = ttk.PanedWindow(self.root, orient=tk.HORIZONTAL)
-        self.main_window.pack(fill=tk.BOTH, expand=True)
+        self.main_window = ttkb.PanedWindow(self.root, orient=ttkb.HORIZONTAL)
+        self.main_window.pack(fill=ttkb.BOTH, expand=True)
         self.sched_frame = ScheduleFrame(self.main_window, self.app)
         self.graph_view = GraphView(self.main_window, self.app)
         self.main_window.add(self.sched_frame, weight=1)
         self.main_window.add(self.graph_view, weight=1)
         self.create_special_buttons()
-    
+
     def create_special_buttons(self):
-        button_frame = ttk.Frame(self.root)
-        button_frame.pack(fill=tk.X, padx=10, pady=10)
-        
-        ttk.Button(
-            button_frame, 
-            text="В конец", 
-            command=self.finish
-        ).pack(side=tk.RIGHT, padx=5)
-        
-        ttk.Button(
-            button_frame, 
-            text="Следующий шаг", 
-            command=self.do_next
-        ).pack(side=tk.RIGHT, padx=5)
-        
-        ttk.Button(
-            button_frame, 
-            text="Предыдущий шаг", 
-            command=self.go_back
-        ).pack(side=tk.RIGHT, padx=5)
-        
-        ttk.Button(
-            button_frame, 
-            text="В начало", 
-            command=self.go_to_start
-        ).pack(side=tk.RIGHT, padx=5)
+        button_frame = ttkb.Frame(self.root)
+        button_frame.pack(fill=ttkb.X, padx=10, pady=10)
+
+        # Добавляем стили к кнопкам
+        ttkb.Button(button_frame, text="В конец", command=self.finish, bootstyle="primary").pack(side=ttkb.RIGHT,
+                                                                                                 padx=5)
+        ttkb.Button(button_frame, text="Следующий шаг", command=self.do_next, bootstyle="primary-outline").pack(
+            side=ttkb.RIGHT, padx=5)
+        ttkb.Button(button_frame, text="Предыдущий шаг", command=self.go_back, bootstyle="secondary-outline").pack(
+            side=ttkb.RIGHT, padx=5)
+        ttkb.Button(button_frame, text="В начало", command=self.go_to_start, bootstyle="info").pack(side=ttkb.RIGHT,
+                                                                                                    padx=5)
         
     def go_to_start(self):
         from startState import StartState
